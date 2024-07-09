70fe062d1daf322b2f75f032d98b312f7a47df9b
diigixa <digixatomique2@gmail.com>
2024-07-09 13:44:57 +0200

    Update articles.router.js

--- api/articles/articles.router.js
+++ api/articles/articles.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ArticleController = require('./articles.controller');
-const authMiddleware = require('../middleware/auth');
+const authMiddleware = require('../middlewares/auth');
 
 router.post('/articles', authMiddleware, ArticleController.createArticle);
 router.put('/articles/:id', authMiddleware, ArticleController.updateArticle);
